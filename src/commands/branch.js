02775610dd8a61515fc557f87930396df419e624
Adam Anderson <adam.anderson@roh.org.uk>
2019-02-21 14:45:01 +0000

    Use request-promise with basic auth for jira

--- src/commands/branch.js
+++ src/commands/branch.js
@@ -1,11 +1,20 @@
 require('colors')
-const { SUCCESS } = require('../constants/log-levels')
+const { INFO, SUCCESS } = require('../constants/log-levels')
 const log = require('../helpers/log')
 const getTicketData = require('../helpers/get-ticket-data')
 
 function branch(ticketId) {
   getTicketData(ticketId)
-    .then(({ data }) => log(ticketId, data, SUCCESS))
+    .then((data) => {
+      const { issuetype, summary } = data.fields
+      const branchName = `${issuetype.name}/${data.key}`.toLowerCase()
+      const name = `${summary}`.replace(/\s+/g, '-').toLowerCase()
+
+      log(`Key: ${data.key}`, INFO)
+      log(`Issue Type: ${issuetype.name}`, INFO)
+      log(`Summary: ${summary}`, INFO)
+      log(`Generated branch name: ${branchName}-${name}`, SUCCESS)
+    })
 }
 
 module.exports = branch
--- src/helpers/get-ticket-data.js
+++ src/helpers/get-ticket-data.js
@@ -1,12 +1,14 @@
-const axios = require('axios')
+const rp = require('request-promise')
 const { api: { authKey, endpoint } } = require('../../config')
 
-const getTicketData = ticketId => axios({
-  url: endpoint,
-  params: {
-    authKey,
-    ticketId,
-  },
-})
+const headers = {
+  Authorization: `Basic ${authKey}`,
+  'Content-Type': 'application/json',
+}
+
+const getTicketData = ticketId => rp({
+  url: `${endpoint}${ticketId}`,
+  headers,
+}).then(data => JSON.parse(data))
 
 module.exports = getTicketData
