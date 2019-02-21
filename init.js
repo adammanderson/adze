3a3494714b1ee9b95fb0878ad80adacc8873d7cd
Adam Anderson <adam.anderson@roh.org.uk>
2019-02-21 13:27:44 +0000

    Add eslint, init file, logs

--- init.js
+++ init.js
@@ -1,1 +1,20 @@
-require('colors');
+require('colors')
+const { copy, existsSync } = require('fs-extra')
+const shell = require('shelljs')
+const { name } = require('./package')
+const log = require('./src/helpers/log');
+const { SUCCESS, INFO, ERROR } = require('./src/constants/log-levels');
+
+shell.exec('npm link')
+
+const configFileExample = 'config.example.json'
+const configFileReal = configFileExample.replace('.example', '')
+
+if (existsSync(configFileReal)) {
+  log(`${name} config already exists`, INFO)
+  process.exit(0)
+}
+
+copy(configFileExample, configFileReal)
+  .then(() => log(`Created ${name} config file`, SUCCESS))
+  .catch(() => log(`Could not create ${name} config file, check the permissions`, ERROR))
