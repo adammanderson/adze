6b95e4fe6d55610324c6705aa6cc9f830a8bcd46
Adam Anderson <adam.anderson@roh.org.uk>
2019-02-21 13:57:37 +0000

    JSON validate config.example, add branch command

--- src/commands/branch.js
+++ src/commands/branch.js
@@ -1,9 +1,11 @@
 require('colors')
 const { SUCCESS } = require('../constants/log-levels')
 const log = require('../helpers/log')
+const getTicketData = require('../helpers/get-ticket-data')
 
 function branch(ticketId) {
-  log(ticketId, SUCCESS)
+  getTicketData(ticketId)
+    .then(({ data }) => log(ticketId, data, SUCCESS))
 }
 
 module.exports = branch
